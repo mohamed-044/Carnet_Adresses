5b739f3e7c8e2b10d4b001e6f2d877b7ba5154dc
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-11-27 17:53:58 +0100

    add class DBConnect with getPDO function that return pdo object

--- DBConnect.php
+++ DBConnect.php
@@ -1,4 +1,6 @@
 <?php
+class DBConnect {
+    public static function getPDO():PDO{
 $host = "localhost";
 $user = "root";
 $password = "";
@@ -11,6 +13,14 @@ try {
         $password,
         [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
     );
+    return $mysqlClient;
 } catch (Exception $e) {
     die("Erreur de connexion : " . $e->getMessage());
 }
+}
+}
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+$pdo = DBConnect::getPDO();
+var_dump($pdo);
