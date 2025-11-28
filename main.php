fa1ec4847f1d8aa1dc659a310c836c0802d46498
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-11-28 18:07:29 +0100

    get contacts from ContactManager, show contacts if the input = list

--- main.php
+++ main.php
@@ -1,8 +1,18 @@
 <?php
+require_once(__DIR__ . '/ContactManager.php');
+$manager = new ContactManager();
+$contacts = $manager->findAll();
 while (true) {
     $line = readline("Entrez votre commande : ");
     if ($line === 'list'){
-        echo "Affichage de la liste\n";}
+        foreach ($contacts as $contact)
+        {
+            echo "Id : {$contact['id']}\n";
+            echo "Nom : {$contact['name']}\n";
+            echo "Email : {$contact['email']}\n";
+            echo "Téléphone : {$contact['phone_number']}\n\n";
+        }
+    }
     else {
         echo "Vous avez saisi : $line\n";
     }
