97bd774aa6492c729e1ab41c3a966ffee4090c9d
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-11-30 20:32:50 +0100

    add help and detail commands

--- main.php
+++ main.php
@@ -1,18 +1,36 @@
 <?php
 require_once(__DIR__ . '/ContactManager.php');
 require_once(__DIR__ . '/Contact.php');
-$manager = new ContactManager();
+$manager = new ContactManager;
 $contacts = $manager->findAll();
+echo "Attention à la syntaxe des commandes, les espaces et les virgules sont importants.\n";
 while (true) {
-    $line = readline("Entrez votre commande : ");
-    if ($line === 'list'){
+    $line = readline("Entrez votre commande (help, list, detail, create, delete, quit) : ");
+    if ($line === 'help') {
+        echo "help : affiche cette aide\n";
+        echo "list : liste des contacts\n";
+        echo "create [name], [email], [phone number] : crée un contact\n";
+        echo "delete [id] : supprime un contact\n";
+        echo "quit : quitte le programme\n";
+    }
+    else if ($line === 'list'){
+        echo "Liste des contacts : \nid, name, email, phone number\n";
         foreach ($contacts as $contact)
         {
-            echo "Id : {$contact['id']}\n";
-            echo "Nom : {$contact['name']}\n";
-            echo "Email : {$contact['email']}\n";
-            echo "Téléphone : {$contact['phone_number']}\n\n";
+            echo "{$contact['id']}, {$contact['name']}, {$contact['email']}, {$contact['phone_number']}\n";
         }
+    }
+    else if (preg_match("/^detail (\d+)$/", $line, $matches)) {
+    $id = intval($matches[1]);
+    $result = $manager->findById($id);
+    if ($result) {
+        echo $result->getId() . ", " .
+         $result->getName() . ", " .
+         $result->getEmail() . ", " .
+         $result->getPhoneNumber() . "\n";
+    } else {
+        echo "Aucun contact trouvé avec l'id $id\n";
+    }
     }
     else {
         echo "Vous avez saisi : $line\n";
