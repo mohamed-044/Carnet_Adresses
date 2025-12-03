a3f28286d8e2d674361f3db654c056f0493acf75
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-12-03 22:32:18 +0100

    use command functions

--- main.php
+++ main.php
@@ -1,62 +1,23 @@
 <?php
-require_once(__DIR__ . '/ContactManager.php');
-require_once(__DIR__ . '/Contact.php');
-$manager = new ContactManager;
-$contacts = $manager->findAll();
+require_once(__DIR__ . '/Command.php');
+$command = new Command;
 echo "Attention à la syntaxe des commandes, les espaces et les virgules sont importants.\n";
 while (true) {
     $line = readline("Entrez votre commande (help, list, detail, create, delete, quit) : ");
     if ($line === 'help') {
-        echo "help : affiche cette aide\n";
-        echo "list : liste des contacts\n";
-        echo "detail [id] : affiche le détail d'un contact\n";
-        echo "create [name], [email], [phone number] : crée un contact\n";
-        echo "delete [id] : supprime un contact\n";
-        echo "quit : quitte le programme\n";
+        $command->help();
     }
     else if ($line === 'list'){
-        echo "Liste des contacts : \nid, name, email, phone number\n";
-        foreach ($contacts as $contact)
-        {
-            echo "{$contact['id']}, {$contact['name']}, {$contact['email']}, {$contact['phone_number']}\n";
-        }
+        $command->list();
     }
-    else if (preg_match("/^detail (\d+)$/", $line, $matches)) {
-    $id = intval($matches[1]);
-    $result = $manager->findById($id);
-    if ($result) {
-        echo $result->getId() . ", " .
-         $result->getName() . ", " .
-         $result->getEmail() . ", " .
-         $result->getPhoneNumber() . "\n";
-    } 
-    else {
-        echo "Aucun contact trouvé avec l'id $id\n";
+    else if ($command->detailCheck($line)) {
+        $command->detail();
     }
+    else if ($command->deleteCheck($line)) {
+        $command->delete();
     }
-    else if (preg_match("/^delete (\d+)$/", $line, $matches)) {
-    $id = intval($matches[1]);
-    $delete = $manager->delete($id);
-    if ($delete) {
-        echo "Contact supprimé\n";
-        $contacts = $manager->findAll();
-    } 
-    else {
-        echo "Le contact n'a pas pu être supprimé\n";
-    }
-    }
-    else if (preg_match("/^create \s*([^,]+),\s*([^,]+),\s*(\S+)$/", $line, $matches)) {
-    $name = trim($matches[1]);
-    $email = trim($matches[2]);
-    $phone_number = trim($matches[3]);
-    $create = $manager->create($name, $email, $phone_number);
-    if ($create) {
-        echo "Contact créé\n";
-        $contacts = $manager->findAll();
-    } 
-    else {
-        echo "Le contact n'a pas pu être créé\n";
-    }
+    else if ($command->createCheck($line)) {
+        $command->create();
     }
     else if ($line === 'quit') {
         exit();
